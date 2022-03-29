892c18a46b6b53891830a2f0819154cf57b11fac
KhaleKuzzaman <s1910979158@ru.ac.bd>
2022-03-29 22:26:37 +0600

    2nd day commit

--- main.c
+++ main.c
@@ -2,46 +2,13 @@
 #include<conio.h>
 #include<stdlib.h>
 
+int number;
 struct Batsmen
 {
     char name[15];
     int total_run;
     int four,six;
 };
-Bat_view()
-{
-    system("CLS");
-    struct Batsmen batsmen[2];
-    printf("\n\n\t\t\t\t*********** BATSMEN VIEW **********");
-    for(int i = 0 ; i<2 ; i++)
-    {
-        printf("\n\n\t# Batsmen Name :");
-        fflush(stdin);
-        gets(batsmen[i].name);
-        printf("\n\t# Total run : ");
-        scanf("%d",&batsmen[i].total_run);
-        printf("\n\t# Total Four & Six : ");
-        scanf("%d%d",&batsmen[i].four,&batsmen[i].six);
-    }
-
-
-
-    printf("\n\n\n\t\t\t\t       *********** BATSMEN VIEW **********");
-    printf("\n\n\t\t\t ----------------------------------------------------------------");
-    printf("\n\t\t\t    Batsman_Name \t Total_runs\t\t_4s\t  _6s");
-    printf("\n\t\t\t ----------------------------------------------------------------\n");
-
-
-    for(int i = 0 ; i<2 ; i++)
-    {
-        printf("\t\t\t     %s \t\t  %d \t\t\t %d \t   %d",batsmen[i].name,batsmen[i].total_run,batsmen[i].four,batsmen[i].six);
-        printf("\n\t\t\t ----------------------------------------------------------------");
-        printf("\n");
-
-    }
-
-    return 0;
-}
 
 main_menu()
 {
@@ -73,7 +40,7 @@ main_menu()
     default :
         printf("\nSorry !!! Your option is not correct");
     }
-
+    return 0;
 
 }
 
@@ -91,7 +58,6 @@ new_score_sheet()
     char toss_win[10];
     char elected[5];
     int inn;
-
 
     printf("\n\t# Competetition name :");
     scanf("%s",&competition);
@@ -149,18 +115,102 @@ new_score_sheet()
 
     }
 
-    Bat_view();
+    return 0;
+}
+Bat_view()
+{
+    system("CLS");
+    printf("\n\n\t\t\t\t*********** BATSMEN SCORE **********");
+    printf("\n\n\t\t Number of Batsmen :");
+    scanf("%d",&number);
+    struct Batsmen batsmen[number];
+    for(int i = 0 ; i<number ; i++)
+    {
+        int sum = 0;
+        int ball[5];
+        printf("\n\n\t# Batsmen Name :");
+        fflush(stdin);
+        gets(batsmen[i].name);
+        printf("\n\n\t %d)%s : ",i+1,batsmen[i].name);
+        for(int j = 0 ; j<6 ; j++)
+        {
+            scanf("%d",&ball[j]);
+            sum = sum + ball[j];
+        }
+        batsmen[i].total_run = sum ;
+        printf("\n\t# Total Four & Six : ");
+        scanf("%d%d",&batsmen[i].four,&batsmen[i].six);
+    }
 
+    int otp;
+    printf("\n\n\n\t\t\t Are you see Batsmen Score View?");
+    printf("\n\t\t\t # Yes for Press 1 :");
+    printf("\n\t\t\t # No for press 2 :");
+    printf("\n\n\t\t\t Enter your press number :");
+    scanf("%d",&otp);
+    switch(otp)
+    {
+    case 1 :
+        system("CLS");
+        for(int l = 0 ; l<number ; l++)
+        {
+            view_score_sheet(batsmen[l].name,batsmen[l].total_run,batsmen[l].four,batsmen[l].six);
+        }
+
+        break;
+    case 2 :
+        system("CLS");
+        Bat_view();
+        break;
+    default :
+        printf("\n\n\n\n\n\t\t\t\t\t Your press number incorrect !!!");
+
+    }
 
     return 0;
 }
 
-view_score_sheet()
+view_score_sheet(char x[15],int r,int f,int s)
 {
+    system("CLS");
     printf("           \n\n");
     printf("\t\t\t\t       *******************************\n");
     printf("\t\t\t\t       * WELCOME TO VIEW SCORE BOARD *\n");
     printf("\t\t\t\t       ***************************** *\n");
+
+    struct Batsmen batsmen[number];
+    printf("\n\n\n\t\t\t\t     *********** BATSMEN VIEW **********");
+    printf("\n\n\t\t\t ----------------------------------------------------------------");
+    printf("\n\t\t\t    Batsman_Name \t Total_runs\t\t_4s\t  _6s");
+    printf("\n\t\t\t ----------------------------------------------------------------\n");
+
+    for(int k=0; k<number; k++)
+    {
+        printf("\t\t\t     %s \t\t  %d \t\t\t %d \t   %d",x,r,f,s);
+        printf("\n\t\t\t ----------------------------------------------------------------");
+        printf("\n");
+
+    }
+    int op;
+    printf("\n\t\t\t # For Main_menu Press 1 :");
+    printf("\n\t\t\t # Exit for press 2 :");
+    printf("\n\n\t\t\t Enter your press number :");
+    scanf("%d",&op);
+    switch(op)
+    {
+    case 1 :
+        system("CLS");
+        main_menu();
+        break;
+    case 2 :
+        system("CLS");
+        exit_score_board();
+        break;
+    default :
+        printf("\n\n\n\n\n\t\t\t\t\t Your press number incorrect !!!");
+
+    }
+
     return 0;
 }
 
@@ -186,12 +236,13 @@ exit_score_board()
         printf("\n\n\n\n\n\t\t\t\t\t Your press number incorrect !!!");
 
     }
-    getch();
+    return 0;
 }
 
 
 int main()
 {
+    system("color f9");
     main_menu();
     getch();
 }
